871803c5fdfedf6a49025a4c982410b1e2e72653
hyesungpark <jayhspark@gmail.com>
2023-11-20 21:55:36 +0900

    first commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Hye Sung Park on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
